1800a238ac841385cff252eed57215b1ebf6cce1
Llostris <deiidrell@gmail.com>
2014-11-21 20:27:37 +0100

    changes to AddLocation View, moved help content to a separate file

--- src/pl/edu/agh/fragments/AddLocationFragment.java
+++ src/pl/edu/agh/fragments/AddLocationFragment.java
@@ -4,14 +4,33 @@ import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;import pl.edu.agh.main.R;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.Spinner;
+import pl.edu.agh.domain.accounts.Address;
+import pl.edu.agh.domain.locations.Location;
+import pl.edu.agh.main.R;
+import pl.edu.agh.tools.RenderingTools;
 
 /**
  * Created by Sławomir on 20.06.14.
  */
 public class AddLocationFragment extends Fragment {
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.add_location_fragment, container, true);
+        View view = inflater.inflate(R.layout.add_location_fragment, container, true);
+
+	    Spinner statusSpinner = (Spinner) view.findViewById(R.id.AddLocation_StatusSpinner);
+	    ArrayAdapter<Location.Status> adapter = new ArrayAdapter<Location.Status>(getActivity(), android.R.layout.simple_spinner_dropdown_item, Location.Status.values());
+	    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+	    statusSpinner.setAdapter(adapter);
+
+	   Button button = ((Button) getActivity().findViewById(R.id.AddLocation_Button));
+
+	    return view;
     }
+
 }
--- src/pl/edu/agh/tools/RenderingTools.java
+++ src/pl/edu/agh/tools/RenderingTools.java
@@ -1,7 +1,9 @@
 package pl.edu.agh.tools;
 
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import pl.edu.agh.main.R;
 
 /**
  * Created by Magda on 2014-10-15.
@@ -11,5 +13,9 @@ public class RenderingTools {
 	public static void setTextViewText(View view, int textViewId, String value) {
 		TextView tvName = (TextView) view.findViewById(textViewId);
 		tvName.setText(value);
+	}
+
+	public static String getValueFromEditText(View view, int editTextId) {
+		return ((EditText) view.findViewById(editTextId)).getText().toString();
 	}
 }
