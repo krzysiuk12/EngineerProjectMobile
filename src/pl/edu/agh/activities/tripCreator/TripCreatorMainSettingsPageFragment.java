cfca5051f0916d1b577f37cc050477b863ccf7d7
Slawex <slawomirbukowiec@gmail.com>
2015-01-02 20:59:57 +0100

    Logika do strony TripCreatorMainSettingsPageFragment.

--- src/pl/edu/agh/activities/tripCreator/TripCreatorMainSettingsPageFragment.java
+++ src/pl/edu/agh/activities/tripCreator/TripCreatorMainSettingsPageFragment.java
@@ -6,12 +6,27 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Spinner;
+import pl.edu.agh.domain.trips.Trip;
 import pl.edu.agh.main.R;
+
+import java.util.Calendar;
+import java.util.Date;
 
 /**
  * Created by Sławek on 2014-12-24.
  */
 public class TripCreatorMainSettingsPageFragment extends TripCreatorWizardPageFragment {
+
+    private EditText tripNameEditText;
+    private EditText tripDescriptionEditText;
+    private DatePicker tripStartDatePicker;
+    private DatePicker tripEndDatePicker;
+    private Spinner distanceUnitSpinner;
+    private Spinner travelModeSpinner;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, final ViewGroup container, Bundle savedInstanceState) {
@@ -35,6 +50,10 @@ public class TripCreatorMainSettingsPageFragment extends TripCreatorWizardPageFr
                 ft.commit();
             }
         });
+
+        Trip trip = new Trip();
+        updateWizardPageFields(view, trip);
+
         return view;
     }
 
@@ -57,5 +76,50 @@ public class TripCreatorMainSettingsPageFragment extends TripCreatorWizardPageFr
         super.onResume();
     }
 
+    private void updateWizardPageFields(View view, Trip trip) {
+        getTripNameEditText(view).setText(trip.getName());
+        getTripDescriptionEditText(view).setText(trip.getDescription());
+        setDatePicker(trip.getStartDate(), getTripStartDatePicker(view));
+        setDatePicker(trip.getEndDate(), getTripEndDatePicker(view));
+    }
+
+    public EditText getTripNameEditText(View view) {
+        if(tripNameEditText == null) {
+            tripNameEditText = ((EditText)view.findViewById(R.id.TripCreatorMainSettingsPage_TripNameEditText));
+        }
+        return tripNameEditText;
+    }
+
+    public EditText getTripDescriptionEditText(View view) {
+        if(tripDescriptionEditText == null) {
+            tripDescriptionEditText = ((EditText)view.findViewById(R.id.TripCreatorMainSettingPage_TripDescriptionEditText));
+        }
+        return tripDescriptionEditText;
+    }
+
+    public DatePicker getTripStartDatePicker(View view) {
+        if(tripStartDatePicker == null) {
+            tripStartDatePicker = ((DatePicker)view.findViewById(R.id.TripCreatorMainSettingsPage_TripStartDatePicker));
+        }
+        return tripStartDatePicker;
+    }
+
+    public DatePicker getTripEndDatePicker(View view) {
+        if(tripEndDatePicker == null) {
+            tripEndDatePicker = ((DatePicker)view.findViewById(R.id.TripCreatorMainSettingsPage_TripEndDatePicker));
+        }
+        return tripEndDatePicker;
+    }
+
+    public void setDatePicker(Date date, DatePicker datePicker) {
+        Calendar calendar = dateToCalendar(date);
+        datePicker.updateDate(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH));
+}
+
+    private Calendar dateToCalendar(Date date) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        return calendar;
+    }
 
 }
